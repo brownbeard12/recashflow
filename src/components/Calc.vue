becada34d64ed8a3e40a6e4e62e588ddbdc2e759
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2023-06-15 11:30:49 -0400

    update

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,63 +1,74 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const pur_price = ref(450000)
-const down_pmt = ref(10000)
-const down_pct = ref(10)
-const closing_pct = ref(2.0)
+const pur_price = ref(450000);
+const down_pmt = ref(10000);
+const down_pct = ref(10);
+const closing_pct = ref(2.0);
 
 function setDown(e) {
-  down_pmt.value = e.target.value * down_pct.value / 100
+  down_pmt.value = (e.target.value * down_pct.value) / 100;
 }
 
 function setDownPmt(e) {
-  down_pmt.value = e.target.value * pur_price.value / 100
+  down_pmt.value = (e.target.value * pur_price.value) / 100;
 }
 
 function setDownPct(e) {
-  down_pct.value = (e.target.value / pur_price.value * 100).toFixed(2)
+  down_pct.value = ((e.target.value / pur_price.value) * 100).toFixed(2);
 }
 
 function makeCurr(val) {
-  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(val)
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(val);
 }
 
 function makePct(val) {
-  return new Intl.NumberFormat('en-US', { style: 'percent', minimumFractionDigits: 2 }).format(val)
+  return new Intl.NumberFormat("en-US", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  }).format(val);
 }
-
 </script>
 
 <template>
   <div class="calc">
     <label>Purchase Price </label><br />
-    <input type="number" step="500" v-model="pur_price" @change="setDown"> {{ makeCurr(pur_price) }}
+    <input type="number" step="500" v-model="pur_price" @change="setDown" />
+    {{ makeCurr(pur_price) }}
     <br />
     <label>Down Payment </label><br />
-    <input type="number" step="50" v-model="down_pmt" @change="setDownPct"> {{ makeCurr(down_pmt) }}
+    <input type="number" step="50" v-model="down_pmt" @change="setDownPct" />
+    {{ makeCurr(down_pmt) }}
     <br />
     <label>Down Payment Percent </label><br />
-    <input type="number" step="0.25" v-model="down_pct" @change="setDownPmt"> {{ makePct(down_pct / 100) }}
+    <input type="number" step="0.25" v-model="down_pct" @change="setDownPmt" />
+    {{ makePct(down_pct / 100) }}
     <br />
     <label>Estimated Closing Cost Percent </label><br />
-    <input type="number" step="0.25" v-model="closing_pct"> {{ makePct(closing_pct / 100) }}
+    <input type="number" step="0.25" v-model="closing_pct" />
+    {{ makePct(closing_pct / 100) }}
     <br />
-    <span>Estimated Closing Costs<br />{{ makeCurr(closing_pct * pur_price / 100)}}</span>
+    <span>Estimated Closing Costs<br />{{
+      makeCurr((closing_pct * pur_price) / 100)
+    }}</span>
   </div>
 </template>
 
-<style>  
+<style>
 input {
-    width: 4.5rem;
-    border-color: blue;
-  }
+  width: 5rem;
+  border-color: blue;
+}
 
-  .calc {
-    line-height: 1.6;
-    margin: 1rem 0;
-    border: 1px solid rgba(var(--accent), 25%);
-    background-color: rgb(250, 250, 250);
-    padding: 1rem;
-    border-radius: 0.4rem;
-  }
+.calc {
+  line-height: 1.6;
+  margin: 1rem 0;
+  border: 1px solid rgba(var(--accent), 25%);
+  background-color: rgb(250, 250, 250);
+  padding: 1rem;
+  border-radius: 0.4rem;
+}
 </style>
